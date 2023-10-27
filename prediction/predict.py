db399319e5c8d98b570c455b32b8e2b48b14c6e8
Dhruv Tyagi <dhruvtyagi203@gmail.com>
2023-10-27 11:40:26 +0530

    second commit

--- prediction/predict.py
+++ prediction/predict.py
@@ -15,7 +15,7 @@ class DogCat:
 
         imagename = self.filename
         # test_image = image.load_img(imagename, target_size = (224,224))
-        test_image = image.load_img(imagename, target_size = (64,64))
+        test_image = image.load_img(imagename, target_size = (224,224))
         test_image = image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis = 0)
         result = np.argmax(model.predict(test_image), axis=1)
